55734690a4f4e6ac763cb210c69eb5bc466fe29e
AlexanderLubbers <alexanderb.lubbers@gmail.com>
2024-06-30 21:21:24 -0400

    got a small start on the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Scioly-Group-Algorithm
+# Science Olympiad Group Algorithm
 This algorithm was created to make putting people into teams and events easier for Science Olympiad.
+# How To Use
---
+++ grouping_algorithm.py
@@ -0,0 +1,17 @@
+from openpyxl import load_workbook
+
+class Grouping_Algorithm:
+    def __init__(self):
+        pass
+
+def getFileName():
+    print("enter in the name of the .xlsl file that you want to generate groups and teams for")
+    userInput = input(".xlsl file with team data: ")
+    return userInput
+
+filename = getFileName()
+
+
+    
+
+
