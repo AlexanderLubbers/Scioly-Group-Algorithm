e238a968ebdf4866a3b24cd41492d9487f37087b
AlexanderLubbers <alexanderb.lubbers@gmail.com>
2024-07-03 20:19:46 -0400

    added the ability to sort people into categories based on event

--- grouping_algorithm.py
+++ grouping_algorithm.py
@@ -1,8 +1,28 @@
 from openpyxl import load_workbook
-
+event1 = {}
+event2 = {}
+event3 = {}
+event4 = {}
+event5 = {}
+event6 = {}
+event7 = {}
+event8 = {}
+event9 = {}
+event10 = {}
+event11 = {}
+event12 = {}
+event13 = {}
+event14 = {}
+event15 = {}
+event16 = {}
+event17 = {}
+event18 = {}
+event19 = {}
+event20 = {}
+event21 = {}
+event22 = {}
+event23 = {}
 class Grouping_Algorithm:
-    def __init__(self):
-        pass
     def getWorkbook(self, filename):
         file = "team_data/" + filename + ".xlsx"
         try:
@@ -12,8 +32,20 @@ class Grouping_Algorithm:
             return False
         sheet = workbook.active
         return sheet
-    def putInEvent(self, people, sheet):
+    def getNumOfTeams(self):
+       pass
+    def putInTeams(self):
         pass
+    def putInEvent(self, people, eventLookup):
+        for key in people:
+            events = people[key]
+            eventList = events.split(',')
+            eventList = [eventList.strip() for eventList in eventList]
+            for t in eventList:
+                for i in eventLookup:
+                    if t == i.get("Event name"):
+                        i["person" + str(len(i))] = key
+        return eventLookup
     def sortSeniors(self, sheet):
         dictionary = {}
         counter = 2
@@ -82,13 +114,36 @@ class Grouping_Algorithm:
             counter = counter + 1
             key = "D" + str(counter)
         return dictionary
-            
-
 
 def getFileName():
     print("enter in the name of the .xlsx file that you want to generate groups and teams for")
     userInput = input(".xlsx file with team data: ")
     return userInput
+def setupEventList(sheet):
+    event1 = {"Event name": sheet["F2"].value}
+    event2 = {"Event name": sheet["F3"].value}
+    event3 = {"Event name": sheet["F4"].value}
+    event4 = {"Event name": sheet["F5"].value}
+    event5 = {"Event name": sheet["F6"].value}
+    event6 = {"Event name": sheet["F7"].value}
+    event7 = {"Event name": sheet["F8"].value}
+    event8 = {"Event name": sheet["F9"].value}
+    event9 = {"Event name": sheet["F10"].value}
+    event10 = {"Event name": sheet["F11"].value}
+    event11 = {"Event name": sheet["F12"].value}
+    event12 = {"Event name": sheet["F13"].value}
+    event13 = {"Event name": sheet["F14"].value}
+    event14 = {"Event name": sheet["F15"].value}
+    event15 = {"Event name": sheet["F16"].value}
+    event16 = {"Event name": sheet["F17"].value}
+    event17 = {"Event name": sheet["F18"].value}
+    event18 = {"Event name": sheet["F19"].value}
+    event19 = {"Event name": sheet["F20"].value}
+    event20 = {"Event name": sheet["F21"].value}
+    event21 = {"Event name": sheet["F22"].value}
+    event22 = {"Event name": sheet["F23"].value}
+    event23 = {"Event name": sheet["F24"].value}
+    return event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12, event13, event14, event15, event16, event17, event18, event19, event20, event21, event22, event23
 
 filename = getFileName()
 algorithm = Grouping_Algorithm()
@@ -98,11 +153,7 @@ if sheet:
     juniors = algorithm.sortJuniors(sheet=sheet)
     sophomores = algorithm.sortSophomores(sheet=sheet)
     freshmen = algorithm.sortFreshman(sheet=sheet)
-    
-    
-#workbook = load_workbook(filename="team_data/Scioly test data.xlsx")
-
-
-    
-
+    event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12, event13, event14, event15, event16, event17, event18, event19, event20, event21, event22, event23 = setupEventList(sheet)
+    eventList = [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12, event13, event14, event15, event16, event17, event18, event19, event20, event21, event22, event23]
+    eventList = algorithm.putInEvent(seniors, eventList)
 
